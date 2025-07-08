868ce8b81080ade0375c95f33e69a2387cb31b46
Wagner Ferreira <wagner-ferreira09@hotmail.com>
2025-07-08 20:24:06 -0300

    Criação do projeto

---
+++ Backend/GestaoRecebiveisAPI.CrossCutting/IoCContainer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace GestaoRecebiveisAPI.CrossCutting
+{
+    public static class IoCContainer
+    {
+        public static IServiceCollection AdicionarServicos(this IServiceCollection services)
+        {
+
+            return services;
+        }
+
+        public static IServiceCollection AdicionarRepositorios(this IServiceCollection services)
+        {
+
+            return services;
+        }
+
+        public static IServiceCollection AdicionarBancoDeDados(this IServiceCollection services, IConfiguration config)
+        {
+            //services.AddDbContext<GestaoRecebiveisContext>(options =>
+            //    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
